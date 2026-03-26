46418e3e609498f6ca8c211158b7a0b7edbe1780
pyr0pizza <andres04jay@gmail.com>
2026-03-26 11:11:43 -0400

    Update txt_adv_main.py

--- txt_adv_main.py
+++ txt_adv_main.py
@@ -4,6 +4,19 @@ import string
 
 #functions
 
+#placeholders most of these will be moved to character database later.
+def Health():
+    pass
+
+def location():
+    pass
+
+def inventory():
+    pass
+
+def save():
+    pass
+
 
 
 #referances
@@ -17,6 +30,14 @@ import string
 def main():
     print("Welcome to Python TxtRPG \n")
 
+#to be added later when saving files is added
+    #print("Choose an Option:")
+    #print("New")
+    #("Load")
+
+
+
+#primary gameplay loop
     while True:
         choice = input("What do you do:  ").strip()
     
